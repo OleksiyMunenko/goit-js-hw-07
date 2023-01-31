93d7ec201002027665e8f1b339a9ef85dfc75dde
OleksiyMunenko <oleksiymun@gmail.com>
2023-01-31 23:02:27 +0200

    item 1 completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryItems(galleryItems) {
+	return galleryItems.map(({preview, original, description}) => {
+		return `
+		 <div class="gallery__item">
+				<a class="gallery__link" href="large-image.jpg">
+					<img
+						class="gallery__image"
+						src="${preview}"
+						data-source="${original}"
+						alt="${description}"
+					/>
+				</a>
+	 		</div> `
+	}).join('');
+
+};
+
